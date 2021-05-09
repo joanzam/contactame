dc2245fce57fa5fcdd0ebe4bdd5150b652f90e6f
Joan Zamora <joanzam@gmail.com>
2021-05-09 15:15:44 -0400

    Actualizacion d datos

--- src-py/main.py
+++ src-py/main.py
@@ -1,1 +1,6 @@
-print(Aqui inicia el programa en python)
+############ V1 # joanzam@gmail.com
+## Correr con version de python > 3
+
+print("Bienvenido, te saluda Joan Zamora, puedes contactarme aquí:")
+print("email: joanzam@gmail.com")
+print("whatsapp: 58 4166142445")
